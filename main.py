e5f3034fbff7aee75cf4ee5e036286400c18bfea
vijay kumar <49490763+rvijaykr10@users.noreply.github.com>
2024-10-10 17:53:15 +0000

    cors added

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 # from typing import Optional
 # from uuid import UUID, uuid4
 import database
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class TodoPost(BaseModel):
     title: str
